67a2e60e143d0147cb38cab08172d2674dbd987d
gongchangyou <gongchangyou@58.com>
2022-06-09 21:00:51 +0800

    seata

--- service/src/main/java/com/mouse/store/DubboDemoApplication.java
+++ service/src/main/java/com/mouse/store/DubboDemoApplication.java
@@ -1,11 +1,12 @@
 package com.mouse.store;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.mouse"})
+@SpringBootApplication(scanBasePackages = {"com.mouse"}, exclude = DruidDataSourceAutoConfigure.class)
 //@EnableNacosDiscovery
 //@DubboComponentScan(value = "com.braindata.dubbodemo.impl")
 @EnableDubbo(scanBasePackages = "com.mouse.store.impl")
